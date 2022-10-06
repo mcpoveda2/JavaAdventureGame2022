6582445c295d70c4ee066ed875717848a7f9e1ba
Justin Riley <20hz20khz@gmail.com>
2022-10-06 09:32:59 -0400

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,16 @@
+/*
+Room
+ - NoArgsConstructor
+ - Room(String _name)
+ - getCharacter()
+ - getItem()
+ - getLocationTo(String direction)
+ - getName()
+ - getPossibleDirections()
+ - linkRoom(Room r, String direction)
+ - setCharacter(Npc character)
+ - setDescription(String d)
+ - setItem(Item i)
+ - setName(String _name)
+ - toString() // returns the description
+*/
