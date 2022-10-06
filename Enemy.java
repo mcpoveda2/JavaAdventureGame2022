4a7f8259ea69a18af116c0233369cd25a0ec1e14
Justin Riley <20hz20khz@gmail.com>
2022-10-06 09:33:55 -0400

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,10 @@
+/*
+Enemy extends Npc
+ - NoArgsConstructor
+ - Enemy(String _name, String _description)
+ - String getAttackName()
+ - int getHealth
+ - int getMagicWeakness()
+ - void loseHealth(int h)
+ - void setAttackName()
+*/
