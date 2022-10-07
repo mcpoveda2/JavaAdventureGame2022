92c7701dd9834011afc4574015dcc708e6673651
Justin Riley <20hz20khz@gmail.com>
2022-10-07 08:00:48 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,14 +1,15 @@
 /*
-Item
- - NoArgsConstructor
- - Item(String _name, String _description)
- - getMagicType()
- - getName()
- - getStrength()
- - setDescription(String d)
- - setName(String _name)
- - isBroken()
- - toString() // returns the description
- - weaken()
+Item (11 tasks)
+ dibs AJ - private instance vars for name, strength, description, int magicType, Random rng
+ dibs AJ + NoArgsConstructor
+ dibs AJ + Item(String _name, String _description)
+ dibs AJ + int getMagicType()
+ dibs EW + String getName()
+ dibs EW + int getStrength()
+ dibs EW + void setDescription(String d)
+ dibs EW + void setName(String _name)
+ dibs WB + isBroken()
+ dibs WB + toString() // returns the description
+ dibs WB + void weaken()
  */
 
