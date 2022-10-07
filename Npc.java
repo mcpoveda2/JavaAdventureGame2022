444b67a94ea63509569e1acff15bbce260020c15
Justin Riley <20hz20khz@gmail.com>
2022-10-07 08:02:38 -0400

    Update Npc.java

--- Npc.java
+++ Npc.java
@@ -1,11 +1,12 @@
 /*
-Npc
- - NoArgsConstructor
- - Npc(String _name, String _description)
- - getName
- - getSpeech
- - setDescription(String d)
- - setName(String _name)
- - setSpeech
- - toString() // returns the description
+Npc (9 tasks)
+ dib KB - private instance vars for name, speech, description
+ dib KB + NoArgsConstructor
+ dib KB + Npc(String _name, String _description)
+ dib KB + String getName
+ dib TC + String getSpeech
+ dib TC + void setDescription(String d)
+ dib TC + void setName(String _name)
+ dib TC + void setSpeech
+ dibs WB + toString() // returns the description
  */
