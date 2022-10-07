13a7dbe3b5f0a3690e30a496a5ef63289a175373
Justin Riley <20hz20khz@gmail.com>
2022-10-07 08:06:15 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,16 +1,17 @@
 /*
-Room
- - NoArgsConstructor
- - Room(String _name)
- - getCharacter()
- - getItem()
- - getLocationTo(String direction)
- - getName()
- - getPossibleDirections()
- - linkRoom(Room r, String direction)
- - setCharacter(Npc character)
- - setDescription(String d)
- - setItem(Item i)
- - setName(String _name)
- - toString() // returns the description
+Room (14 tasks)
+ dibs SW - private instance vars for name, description, character, roomItem, Room north, Room south, Room east, Room west
+ dibs SW + NoArgsConstructor
+ dibs SW + Room(String _name)
+ dibs SW + Npc getCharacter()
+ dibs AB + Item getItem()
+ dibs AB + Room getLocationTo(String direction)
+ dibs AB + String getName()
+ dibs AB + String getPossibleDirections()
+ + void linkRoom(Room r, String direction)
+ + void setCharacter(Npc character)
+ + void setDescription(String d)
+ + void setItem(Item i)
+ + void setName(String _name)
+ + toString() // returns the description
 */
